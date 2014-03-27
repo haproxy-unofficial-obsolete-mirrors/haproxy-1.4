rahulagarwal30/haproxy

BUG/MEDIUM: http: don't start to forward request data before the connect
Currently, "balance url_param check_post" randomly works. If the client
sends chunked data and there's another chunk after the one containing the
data, http_request_forward_body() will advance msg->sov and move the start
of data to the beginning of the last chunk, and get_server_ph_post() will
not find the data.

In order to avoid this, we add an HTTP_MSGF_WAIT_CONN flag whose goal is
to prevent the forwarding code from parsing until the connection is
confirmed, so that we're certain not to fail on a redispatch. Note that
we need to force channel_auto_connect() since the output buffer is empty
and a previous analyser might have stopped auto-connect.

The flag is currently set whenever some L7 POST analysis is needed for a
connect() so that it correctly addresses all corner cases involving a
possible rewind of the buffer, waiting for a better fix.

Note that this has been broken for a very long time. Even all 1.4 versions
seem broken but differently, with ->sov pointing to the end of the arguments.
So the fix should be considered for backporting to all stable releases,
possibly including 1.3 which works differently.

src/proto_http.c
@@ -4883,8 +4883,17 @@ int http_request_forward_body(struct session *s, struct channel *req, int an_bit
 		return 1;
 	}
 
-	/* in most states, we should abort in case of early close */
-	channel_auto_close(req);
+	/* Some post-connect processing might want us to refrain from starting to
+	 * forward data. Currently, the only reason for this is "balance url_param"
+	 * whichs need to parse/process the request after we've enabled forwarding.
+	 */
+	if (unlikely(msg->flags & HTTP_MSGF_WAIT_CONN)) {
+		if (!(s->rep->flags & CF_READ_ATTACHED)) {
+			channel_auto_connect(req);
+			goto missing_data;
+		}
+		msg->flags &= ~HTTP_MSGF_WAIT_CONN;
+	}
 
 	/* Note that we don't have to send 100-continue back because we don't
 	 * need the data to complete our job, and it's up to the server to
@@ -4905,6 +4914,9 @@ int http_request_forward_body(struct session *s, struct channel *req, int an_bit
 		else
 			msg->msg_state = HTTP_MSG_DATA;
 	}
+
+	/* in most states, we should abort in case of early close */
+	channel_auto_close(req);
 
 	while (1) {
 		unsigned int bytes;

haproxy-unofficial-obsolete-mirrors/haproxy-1.4

(cherry picked from commit 80a92c02f478dc1b836e0c97c891875437fc54da)
Note for 1.4:
-------------
There is no http_msg->flags, and txn->flags is full, so instead the
flag was added to the session's flags, which is acceptable for a
maintenance branch.

src/proto_http.c
@@ -4169,7 +4169,7 @@ void http_end_txn_clean_session(struct session *s)
 	s->req->flags &= ~(BF_SHUTW|BF_SHUTW_NOW|BF_AUTO_CONNECT|BF_WRITE_ERROR|BF_STREAMER|BF_STREAMER_FAST|BF_NEVER_WAIT);
 	s->rep->flags &= ~(BF_SHUTR|BF_SHUTR_NOW|BF_READ_ATTACHED|BF_READ_ERROR|BF_READ_NOEXP|BF_STREAMER|BF_STREAMER_FAST|BF_WRITE_PARTIAL|BF_NEVER_WAIT);
 	s->flags &= ~(SN_DIRECT|SN_ASSIGNED|SN_ADDR_SET|SN_BE_ASSIGNED|SN_FORCE_PRST|SN_IGNORE_PRST);
-	s->flags &= ~(SN_CURR_SESS|SN_REDIRECTABLE);
+	s->flags &= ~(SN_CURR_SESS|SN_REDIRECTABLE|SN_WAIT_CONN);
 	s->txn.meth = 0;
 	http_reset_txn(s);
 	s->txn.flags |= TX_NOT_FIRST | TX_WAIT_NEXT_RQ;
@@ -4575,6 +4575,18 @@ int http_request_forward_body(struct session *s, struct buffer *req, int an_bit)
 	}
 
 	buffer_dont_close(req);
+
+	/* Some post-connect processing might want us to refrain from starting to
+	 * forward data. Currently, the only reason for this is "balance url_param"
+	 * whichs need to parse/process the request after we've enabled forwarding.
+	 */
+	if (unlikely(s->flags & SN_WAIT_CONN)) {
+		if (!(s->rep->flags & BF_READ_ATTACHED)) {
+			buffer_auto_connect(req);
+			goto missing_data;
+		}
+		s->flags &= ~SN_WAIT_CONN;
+	}
 
 	/* Note that we don't have to send 100-continue back because we don't
 	 * need the data to complete our job, and it's up to the server to